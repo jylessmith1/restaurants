b0a57962595baa8916b26271eda8ee3ae048c805
COR Coding <corcoding@CORs-MacBook-12.localdomain>
2023-04-25 11:47:50 -0400

    few changes got data back from database

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const usersRouter = require('./routes/users');
 
-const usersRouter = require('./routes/employees');
-const usersRouter = require('./routes/menuItems');
-const usersRouter = require('./routes/reservations');
-const usersRouter = require('./routes/tables');
+const employeesRouter = require('./routes/employees');
+const menuItemsRouter = require('./routes/menuItems');
+const reservationsRouter = require('./routes/reservations');
+const tablesRouter = require('./routes/tables');
 
 
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 app.use('/employees', employeesRouter);
 app.use('/menuItems', menuItemsRouter);
--- routes/menuItems.js
+++ routes/menuItems.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const { menuItems } = require('../models');
 
-/* GET users listing. */
-router.get('/menuItems', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+    const menuItem = await menuItems.findAll()
+    res.json(menuItem);
+
 });
+router.get('/:id', async (req, res) => {
+    const menuItem = await menuItems.findAll()
+    const { id } = req.params
+
+    menuItem.forEach(Item => {
+        if(id == Item.id) {
+            res.json(Item)
+        }
+    });
+    res.json(menuItems);
+    console.log(id)
+
+});
+
+router.post('/', async (req, res) => {
+    const menuItem = await menuItems.findAll()
+    const newMenuItem = {
+        name: req.body.name,
+        price: req.body.price 
+    }
+    menuItem.push(newMenuItem)
+})
 
 module.exports = router;
